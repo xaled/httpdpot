e9023686c4e279480171957a2b3e27e0d6bfcaf5
xaled <kh.grandi@gmail.com>
2018-09-20 13:14:49 +0100

    fix httpdpot-install.sh

--- scripts/httpdpot-install.sh
+++ scripts/httpdpot-install.sh
@@ -5,5 +5,5 @@ echo_and_run() { echo "$@" ; "$@" ; }
 echo_and_run mkdir /opt/httpdpot &&
 echo_and_run mkdir /opt/httpdpot/repo  &&
 echo_and_run git clone https://github.com/xaled/httpdpot /opt/httpdpot/repo &&
-echo_and_run cp -R /opt/httpdpot/repo/scripts  var/httpdpot/ &&
+echo_and_run cp -R /opt/httpdpot/repo/scripts  /opt/httpdpot/ &&
 echo_and_run /opt/httpdpot/scripts/httpdpot.enable.sh
